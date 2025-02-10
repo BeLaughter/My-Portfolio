dc66858ba6186285ed2cc3e538dc97c96d970d85
Tufem Gift Items <Laughteriremide@gmail.com>
2025-02-10 13:01:16 +0100

    my changes

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,32 +1,35 @@
-import {
-  FaFacebook,
-  FaInstagram,
-  FaLinkedin,
-  FaWhatsapp,
-} from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 import "./Body.css";
+import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function Body() {
+  // Initialize AOS
+  useEffect(() => {
+    Aos.init();
+  }, []);
   return (
     <div className="body">
       <div className="flex-container">
         <div className="flex-itemb">
           <p className="bodyp1">Hi, </p>
           <h1 className="bodyh1">
-            I'm{" "}
-            <span className="orange">
+            I'm
+            <span
+              className="orange"
+              data-aos="zoom-in-right"
+              data-aos-duration="500"
+            >
               Benson <br></br>Iremide
             </span>
           </h1>
-          <p className="bodyp2">
+          <p className="bodyp2 ">
             I'm a Frontend Web Developer based in Lagos. With over 2 years of
             experience in creating responsive, user-friendly web applications, I
             specialize in React.js . Explore my portfolio to see my work and get
             to know more about my skills and projects.
           </p>
-          <a href="#" target="blank">
-            <FaFacebook className="iconsz" />
-          </a>
           <a href="http://Instagram.com/that.laughter" target="blank">
             <FaInstagram className="iconsz" />
           </a>
@@ -39,9 +42,9 @@ function Body() {
         </div>
         <div className="flex-itemb">
           <img
-            src="https://cdn.pixabay.com/photo/2022/08/24/15/42/library-7408106_1280.jpg"
+            src="https://i.ibb.co/q30G4jWC/IMG-6412.jpg"
             alt="Image 3"
-            className="d-block w-100"
+            className="d-block w-100 imgh"
           ></img>
         </div>
       </div>
--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -7,24 +7,24 @@ function Project() {
       <div className="flex-container">
         <div className="flex-itemb">
           <Projectcard
-            img="https://cdn.pixabay.com/photo/2020/08/18/19/25/teacher-5499177_640.jpg"
+            img="https://i.ibb.co/MDrx8v4c/IMG-6420.jpg"
             lang=" Html"
             lang2=" Css"
             lang3=" Bootstrap"
             text="Tech school webpage built with react.js, html, css & bootstrap"
             link1="https://boldlink-web.vercel.app"
-            link2="#"
+            link2="https://github.com/BeLaughter/Boldlink-web/tree/main"
           />
         </div>
         <div className="flex-itemb">
           <Projectcard
-            img="https://cdn.pixabay.com/photo/2020/08/18/19/25/teacher-5499177_640.jpg"
+            img="https://i.ibb.co/jkQkn4N4/IMG-6415.jpg"
             lang="Html"
             lang2="Css"
             lang3="JavaScript"
             text="Calculator built with Html css and JavaScript"
             link1="https://calculator-seven-black-85.vercel.app"
-            link2="#"
+            link2="https://github.com/BeLaughter/calculator/tree/main"
           />
         </div>
       </div>
@@ -32,24 +32,24 @@ function Project() {
       <div className="flex-container">
         <div className="flex-itemb">
           <Projectcard
-            img="https://cdn.pixabay.com/photo/2020/08/18/19/25/teacher-5499177_640.jpg"
+            img="https://i.ibb.co/S4WS1SJ9/IMG-6416.jpg"
             lang=" Html"
             lang2=" Css"
             lang3=" Bootstrap"
             text="React practice webpage built with react.js, html, css & bootstrap"
             link1="https://react-gilt-beta.vercel.app/"
-            link2="#"
+            link2="https://github.com/BeLaughter/react/tree/main"
           />
         </div>
         <div className="flex-itemb">
           <Projectcard
-            img="https://cdn.pixabay.com/photo/2020/08/18/19/25/teacher-5499177_640.jpg"
+            img="https://i.ibb.co/hJDtyzqx/IMG-6419.jpg"
             lang="Html"
             lang2="Javascript"
             lang3="Css"
             text="Number guess game"
             link1="https://guessdnum.vercel.app"
-            link2="#"
+            link2="https://github.com/BeLaughter/Guessdnum/tree/main"
           />
         </div>
       </div>
@@ -57,24 +57,24 @@ function Project() {
       <div className="flex-container">
         <div className="flex-itemb">
           <Projectcard
-            img="https://cdn.pixabay.com/photo/2020/08/18/19/25/teacher-5499177_640.jpg"
+            img="https://i.ibb.co/zHmSzcd5/IMG-6418.jpg"
             lang=" Html"
             lang2=" Css"
             lang3=" Bootstrap"
             text="Company webpage built with html, css & bootstrap"
             link1="https://1stweb-eight.vercel.app"
-            link2="#"
+            link2="https://github.com/BeLaughter/web/tree/main"
           />
         </div>
         <div className="flex-itemb">
           <Projectcard
-            img="https://cdn.pixabay.com/photo/2020/08/18/19/25/teacher-5499177_640.jpg"
-            lang="MR BELLO"
-            lang2="MR BELLO"
-            lang3="MR BELLO"
+            img="https://i.ibb.co/fzSrtYPW/IMG-6417.jpg"
+            lang="Html"
+            lang2="Css"
+            lang3="JavaScript"
             text="Form page with form validation built with javascript css and html"
             link1="https://form-bay-pi.vercel.app"
-            link2="#"
+            link2="https://github.com/BeLaughter/form/tree/main"
           />
         </div>
       </div>
